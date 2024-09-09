e77d4dff44e5023d7f752d2a2afa5bcd9363e1ea
Andrés Haskel <andres.haskel@gmail.com>
2024-09-08 22:36:27 -0300

    feat: routing refactor to work with pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
+import 'dropzone/dist/dropzone.css';
+import 'flatpickr/dist/flatpickr.min.css';
+import 'jsvectormap/dist/css/jsvectormap.css';
+import 'nouislider/dist/nouislider.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
-import './css/style.css';
 import './css/satoshi.css';
 import './css/simple-datatables.css';
-import 'jsvectormap/dist/css/jsvectormap.css';
-import 'flatpickr/dist/flatpickr.min.css';
-import 'nouislider/dist/nouislider.css';
-import 'dropzone/dist/dropzone.css';
+import './css/style.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
